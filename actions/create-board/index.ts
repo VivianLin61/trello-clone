a8f2a04c29c272e3835d6fde8955041e0402b9d2
Vivian Lin <linvivian61@gmail.com>
2024-01-11 17:19:26 -0500

    create board audit log

--- actions/create-board/index.ts
+++ actions/create-board/index.ts
@@ -5,9 +5,11 @@ import { revalidatePath } from "next/cache";
 
 import { db } from "@/lib/db";
 import { createSafeAction } from "@/lib/create-safe-action";
-
+import { createAuditLog } from "@/lib/create-audit-log";
+import { ACTION, ENTITY_TYPE } from "@prisma/client";
 import { InputType, ReturnType } from "./types";
 import { CreateBoard } from "./schema";
+import { Prisma } from "@prisma/client";
 
 const handler = async (data: InputType): Promise<ReturnType> => {
   const { userId, orgId } = auth();
@@ -28,6 +30,13 @@ const handler = async (data: InputType): Promise<ReturnType> => {
         title,
         orgId,
       },
+    });
+
+    await createAuditLog({
+      entityTitle: board.title,
+      entityId: board.id,
+      entityType: ENTITY_TYPE.BOARD,
+      action: ACTION.CREATE,
     });
   } catch (error) {
     console.log(error);
